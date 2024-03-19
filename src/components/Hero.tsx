db634066fd54e41e193d3a231f69fd59b6c46d4f
Harshh1104 <verma.harsh1512@gmail.com>
2024-03-19 12:17:35 +0530

    updating homepage

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,9 @@
-import hero from "../assets/hero.png"
+import hero from "../assets/hero.jpg"
 
 const Hero = ()=> {
     return (
         <div>
-            <img src={hero} className="w-full max-h-[600px] object-cover"/>
+            <img src={hero} className="w-full max-h-[700px] object-cover"/>
         </div>
     )
 }
